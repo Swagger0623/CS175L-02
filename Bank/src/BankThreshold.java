6649f780d6173e06307003b47c82f0214de547eb
neil_swagger <neil_swagger@localhost>
2021-03-29 00:37:00 -0400

    nope

---
+++ Bank/src/BankThreshold.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class BankThreshold {
+
+public static void main(String[] args) {
+	double bal = 0;
+	double interest = 0;
+	double target = 0;
+Scanner in = new Scanner(System.in);
+//insert start bal
+//scanner here for bal named inputbal
+//insert interst
+//scanner input interest 
+//interest target
+//scanner inputtarget
+
+	while(bal < target)
+	{double Intt = bal* interest;
+	bal=bal + Intt;
+	months++;
+	System.out.println(months);
+
+		}
+	}
+}
