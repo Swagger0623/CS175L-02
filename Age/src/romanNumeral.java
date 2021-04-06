b1c66eb7b3dd2d757280f91ec8c6cc8c082b0d0c
neil_swagger <neil_swagger@localhost>
2021-04-05 23:00:16 -0400

    resubmit

--- Age/src/romanNumeral.java
+++ Age/src/romanNumeral.java
@@ -2,24 +2,83 @@ import javax.swing.JOptionPane;
 
 public class romanNumeral {
 	
-	public static void main(String[] args) {
-		String[]  romanNumeralOnes = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-		String[]  romanNumeralTens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-		String[]  romanNumeralHundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+	public static void main(String[] args)
+	{
+		String romanNumeralYear = "";
 		
 		int yearInput = Integer.parseInt(JOptionPane.showInputDialog("Enter the year: "));
 		System.out.println(yearInput);
 		
-		int millenia = yearInput/1000%10;
-		int centuries = yearInput/100%10;
-		int decades = yearInput/10%10;
-		int years = yearInput/1%10;
 		
-	System.out.println(millenia + " " + centuries + " " + decades + " " + years);
+			while (yearInput >= 10000) {
+				romanNumeralYear += "X";
+				yearInput -= 10000;
+			}
+	        while (yearInput >= 9000) {
+				romanNumeralYear += "MX";
+				yearInput -= 9000;
+	        }
+	        // 5000 is V with a line over the V but it wouldn't let me insert that 
+			while (yearInput >= 5000) {
+				romanNumeralYear += "V";
+		        yearInput -= 5000;
+		    }
+			while (yearInput >= 4000) {
+				romanNumeralYear += "MV";
+		        yearInput -= 4000;
+		    }
+			while (yearInput >= 1000) {
+			 romanNumeralYear += "M";
+		        yearInput -= 1000;        }
+		    while (yearInput >= 900) {
+		    	romanNumeralYear += "CM";
+		        yearInput -= 900;
+		    }
+		    while (yearInput >= 500) {
+		    	romanNumeralYear += "D";
+		        yearInput -= 500;
+		    }
+		    while (yearInput >= 400) {
+		    	romanNumeralYear += "CD";
+		        yearInput -= 400;
+		    }
+		    while (yearInput >= 100) {
+		    	romanNumeralYear += "C";
+		        yearInput -= 100;
+		    }
+		    while (yearInput >= 90) {
+		    	romanNumeralYear += "XC";
+		        yearInput -= 90;
+		    }
+		    while (yearInput >= 50) {
+		    	romanNumeralYear += "L";
+		        yearInput -= 50;
+		    }
+		    while (yearInput >= 40) {
+		    	romanNumeralYear += "XL";
+		        yearInput -= 40;
+		    }
+		    while (yearInput >= 10) {
+		    	romanNumeralYear += "X";
+		        yearInput -= 10;
+		    }
+		    while (yearInput >= 9) {
+		    	romanNumeralYear += "IX";
+		        yearInput -= 9;
+		    }
+		    while (yearInput >= 5) {
+		    	romanNumeralYear += "V";
+		        yearInput -= 5;
+		    }
+		    while (yearInput >= 4) {
+		    	romanNumeralYear += "IV";
+		        yearInput -= 4;
+		    }
+		    while (yearInput >= 1) {
 	
-	String romanNumeralYear = "M".repeat(millenia)+romanNumeralHundreds[ centuries]+romanNumeralTens[decades]+romanNumeralOnes[years];
 	System.out.println(romanNumeralYear);
 	
 	JOptionPane.showMessageDialog(null, "The roman numerals for the year " + yearInput + "is" +romanNumeralYear);
 	}
+}
 }
